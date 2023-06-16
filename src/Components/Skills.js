374583ad9341900d6b5ea71da4e2be565ca05aa8
bahargumuser <121288876+bahargumuser@users.noreply.github.com>
2023-06-16 22:02:18 +0300

    last changes

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -11,7 +11,7 @@ const Skills = () => {
   return (
     <section
       className="section-skills"
-      class="min-h-screen items-center bg-[#cbf281]"
+      class="min-h-[80vh] md:min-h-screen items-center bg-[#cbf281] pl-3"
     >
       <h2
         class="header-skills"
