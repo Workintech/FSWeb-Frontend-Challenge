cf4e28205c68282a6ae5e0a25bd8877ee6d0e6cd
Bahar Gümüşer <121288876+bahargumuser@users.noreply.github.com>
2023-11-01 20:55:59 +0300

    designment change

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,34 +1,37 @@
 import React from "react";
 import headerPic from "../assets/a.JPG";
-
 import Button from "../Utils/Button";
 
 const Header = () => {
   return (
-    <section className="min-h-screen bg-violet-300 p-20 m-0 rounded items-center flex flex-3 lg:flex-row flex-col-reverse">
-      <div className=" flex flex-2 flex-col justify-start">
-        <header className="underline font-mono text-center lg:text-left text-[#cbf281] text-[22px] font-bold py-14">
-          Bahar Gümüşer
-        </header>
+    <div className="w-screen h-screen flex justify-center items-center bg-violet-300 lg:w-screen">
+    <section className="min-h-screen bg-violet-300 p-20 rounded items-center flex h-2/3 lg:flex-row flex-col-reverse justify-center items-center">
+      <div className="space-y-16 pt-25 mt-10 pb-4">
         <div>
-          <h1 className="font-mono text-center lg:text-left text-5xl text-[#cbf281] font-bold pb-10">
-            I'm a Full Stack Developer...
-          </h1>
-          <p className="font-mono font-[12px] leading-7 text-center lg:text-left pt-10  ">
-            Hello, if you are looking for a junior developer and my profile
-            interests you, you can contact me!
-          </p>
-
-          <Button />
-        </div>
-      </div>
-      <div className="flex-1 flex justify-center">
+        <div className="absolute top-0 left-1/2 transform -translate-x-1/2 pt-16">
         <img
           src={headerPic}
-          className="max-w-[12rem] aspect-auto drop-shadow-xl rounded-[2rem]"
+          className="max-w-[12rem] aspect-auto drop-shadow-xl rounded-[2rem] pb-4 hover:scale-110 "
         />
       </div>
+      <div className="pb-12"></div>
+  
+          <h1 className="font-mono   pt-16 text-center underline text-2xl lg:text-5xl text-[#cbf281] font-bold ">
+            I'm a Full Stack Developer
+          </h1>
+          <p className="font-mono  pt-16 leading-7 text-center text-sm lg:text-base">
+          Hello, I am Bahar Gumuser. If you are looking for a developer and my profile interests you, you can contact me!"
+          </p>
+          <div className="flex justify-center items-center pt-16 rounded-xl drop-shadow-2xl border-solid border-slate-800  ">
+          <Button/>
+          </div>
+        </div>
+     
+     
+      </div>
     </section>
+    
+    </div>
   );
 };
 export default Header;
--- src/Utils/Button.js
+++ src/Utils/Button.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const Button = () => {
   return (
-    <div className="flex pt-8 justify-center lg:justify-start space-x-4 ">
-      <button mr-2>
+    <div className="flex  justify-center lg:justify-center space-x-4 ">
+      <button>
         <a
           href="https://www.linkedin.com/in/bahar-gumuser-83b0801b2/"
           target="_blank"
-          className="px-5 py-1 rounded-xl drop-shadow-2xl border-solid  border-slate-800 hover:bg-[#cbf281] bg-white"
+          className="px-5 py-3 rounded-xl drop-shadow-2xl border-solid  border-slate-800 hover:bg-[#cbf281] bg-white"
         >
           <i class="fa-brands fa-linkedin-in"></i>
         </a>
@@ -16,7 +16,7 @@ const Button = () => {
         <a
           href="https://github.com/bahargumuser"
           target="_blank"
-          className="px-5 py-1 rounded-xl drop-shadow-2xl border-solid  border-slate-800 hover:bg-[#cbf281] bg-white"
+          className="px-5 py-3 rounded-xl drop-shadow-2xl border-solid  border-slate-800 hover:bg-[#cbf281] bg-white"
         >
           <i class="fa-brands fa-github"></i>
         </a>
