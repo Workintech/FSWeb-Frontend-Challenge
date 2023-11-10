b6d7f907c7e75e6d8abe292b173d52cc88221c59
Bahar Gümüşer <121288876+bahargumuser@users.noreply.github.com>
2023-11-10 17:45:56 +0300

    content

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -5,40 +5,17 @@ import "../CSS/profile.css";
 const Profile = () => {
   return (
     <section className="section-profile">
+       <h1>About Me</h1>
+      
       <div className="profile-left">
-        <h1>Profile</h1>
-        <h2>Basic Information</h2>
-        <p>
-          <span>Birthyear: </span>1996{" "}
-        </p>
-        <p>
-          <span>Country: </span>Turkey
-        </p>
-        <p>
-          <span>E-mail: </span>bahargumuser@gmail.com
-        </p>
-        <p>
-          <span>Educational Status: </span>Ankara University, 2021{" "}
-        </p>
-        <p>
-          <span>Preferred Job: </span> Junior Developer
-        </p>
-      </div>
-      {/*       <img src={profilePic} alt="profile" id="profilePic" /> */}
 
-      <div className="About-Me">
-        <h3 /* className="tc-[#cbf281]" */>About Me</h3>
-        <p>
-          I am a person who has built my life on new information and new
-          discoveries. After graduating from university, I became interested in
-          software and wanted to learn new things.{" "}
+        <p>During my educational journey within the discipline of mechanical engineering and in the subsequent period, I developed projects using Arduino, Ansys, and SolidWorks.</p>
+        <p>Later on, I pursued training in web development and began utilizing JavaScript, React, and Node.js technologies. As I delved deeper, software development evolved into a passion for me. Now, I have made the decision to align this passion with my career goals. I am dedicated to working a bit harder every day to achieve success.
         </p>
-        <p>
-          Over time, software became a passion for me. I used this passion for
-          my professional purposes and decided to turn it into a business. I
-          improve myself a little bit more every day to be successful.
-        </p>
+        
       </div>
+   
+     
     </section>
   );
 };
