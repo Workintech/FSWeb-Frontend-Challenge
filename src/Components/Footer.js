dc794173efffe1847b5cf0bae512117cde2ebca2
Bahar Gümüşer <121288876+bahargumuser@users.noreply.github.com>
2023-11-10 18:05:19 +0300

    pass on tailwind

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,33 +1,30 @@
 import React from "react";
-import "../CSS/footer.css";
+
 
 const Footer = () => {
   return (
-    <section
-      className="section-footer"
-      /* className="text-mono w-full h-96 text-2xl m-24 text-center" */
-    >
-      <div className="kare">
-        <p>Got a question or proposal, or just want to say hello?</p>
-        <p>Go ahead,</p>
-        <h2 id="sendMeMsc">Send me a message!</h2>
+    <div className="section-footer">
+    <section className="w-screen h-screen flex justify-center  items-center  ">
+      <div className="font-mono text-center text-l lg:text-xl text-[#373737] lg:w-screen justify-center items-center w-screen ">
+        <p className="">Got a question or proposal, or just want to say hello?</p>
+        <p className="mt-4">Go ahead,</p>
+        <h2 className="underline decoration-solid mt-12 lg:mt-4" >Send me a message!</h2>
+        <div class="scale-150 font-mono pt-8 mt-[60px] lg:mt-2 text-center text-xl lg:text-2xl text-[#a5b4fc] font-bold ">
 
-        <div class="email">
-          <div className="icons">
-            <a
-              href="https://www.linkedin.com/in/bahar-gumuser-83b0801b2/"
-              target="_blank"
-            >
-              <i class="fa-brands fa-linkedin"></i>
+          <div className="scale-150 mr-2 ">
+            <a href="https://www.linkedin.com/in/bahar-gumuser-83b0801b2/" target="_blank">
+              <i class="fa-brands fa-linkedin " ></i>
             </a>
-
             <a href="https://github.com/bahargumuser" target="_blank">
-              <i class="fa-brands fa-square-github"></i>
+              <i class="fa-brands fa-square-github ml-2"></i>
             </a>
+       
           </div>
         </div>
       </div>
+     
     </section>
+    </div>
   );
 };
 export default Footer;
