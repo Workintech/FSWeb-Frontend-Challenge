b32f2afa8184d01f7f3198d905bb937b46d91522
Bahar Gümüşer <121288876+bahargumuser@users.noreply.github.com>
2023-11-10 17:40:33 +0300

    add inclusive tailwind

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,10 +4,13 @@ import ProjectsArray from "../Utils/ProjectsArray";
 
 const Profile = () => {
   return (
+    <div className="text-center">
     <section className="section-project">
-      <h1 className="section-profile-header"> Some Projects...</h1>
+    <h1 className="section-profile-header">A Few of My Projects</h1>
       <ProjectsArray />
+     
     </section>
+    </div>
   );
 };
 export default Profile;
