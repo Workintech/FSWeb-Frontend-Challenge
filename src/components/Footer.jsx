6e18a6e27f3cf2485ece008e7d25a1bde3f42ad6
ilsu-sunal <sunalilsu@gmail.com>
2024-03-10 23:41:23 +0300

    Footer css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,16 @@ export default function Footer (){
     //Buradaki içerik maple yazılacak
     return(
         <>
-        <p className="text-4xl mt-4 mb-8">Let’s work together on your next product.</p>
-        <div>
-            <a href="">Github</a>
-            <a href="">Personel Blog</a>
-            <a href="">Linkedin</a>
-            <a href="">Email</a>
-        </div>
+        <main className="px-10 py-10 md:px-40 md:py-20  md:grid md:grid-cols-3 gap-10">
+            <p className="col-span-2 text-3xl md:text-4xl mt-4 md:mb-8 tracking-wide leading-normal font-medium md:pl-20 text-right pr-10">Let’s work together on your next product.</p>
+            <div className="flex justify-around gap-4 mt-4 md:flex-col md:justify-center font-medium">
+                <a className="text-sky-600" href="">Github</a>
+                <a className="text-black" href="">Personal Blog</a>
+                <a className="text-cyan-800" href="">Linkedin</a>
+                <a className="text-pink2" href="">Email</a>
+            </div> 
+        </main>
+        
         </>
     )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,8 @@ export default function Projects (){
         <>
         <main className="px-10 py-10 md:px-40 md:py-20 flex flex-col items-center">
             <h2 className="text-4xl tracking-wide font-medium pb-10">Projects</h2>
-            <section className="bg-bluecard rounded-2xl w-full px-6 py-8">
+            <div className=" grid md:grid-cols-2 gap-10">
+               <section className="bg-bluecard rounded-2xl w-full px-6 py-8">
                 <h2 className="text-3xl font-medium py-4">Proje Başlığı</h2>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia.</p>
                 <div className="flex flex-wrap gap-2 py-4">
@@ -23,6 +24,25 @@ export default function Projects (){
                 <img src="" alt="" />
                 <img className="py-8" src={computerImage} alt="" />
             </section>
+            <section className="bg-greencard rounded-2xl w-full px-6 py-8">
+                <h2 className="text-3xl font-medium py-4">Proje Başlığı</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia.</p>
+                <div className="flex flex-wrap gap-2 py-4">
+                    <p className="bg-white rounded-3xl py-2 px-3">tag11111</p>
+                    <p className="bg-white rounded-3xl py-2 px-3">tag2222</p>
+                    <p className="bg-white rounded-3xl py-2 px-3">tag33333</p>
+                    <p className="bg-white rounded-3xl py-2 px-3">tag44444</p>
+                    <p className="bg-white rounded-3xl py-2 px-3">tag55555</p>
+                </div>
+                <div className="flex justify-between text-xl font-medium py-4">
+                    <a href="">View on Github</a>
+                    <a href="">Go to App -</a>
+                </div>
+                <img src="" alt="" />
+                <img className="py-8" src={computerImage} alt="" />
+            </section> 
+            </div>
+            
         </main>
             
         </>
