7dc651964368a6aef50d4d439f752511963dbaf4
ilsu-sunal <sunalilsu@gmail.com>
2024-03-12 20:25:38 +0300

    emoji eklendi

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,12 @@
-import React, { useContext, useState } from "react"
-import linkedinLogo from "../assets/linkedin.svg"
-import githubLogo from "../assets/github.svg"
-import profilePic from "../assets/images/foto.jpg"
-import { LanguageContext } from "../contexts/LanguageContext"
-import { useDarkMode } from "../contexts/DarkModeContext"
+import React, { useContext} from "react";
+
+import { LanguageContext } from "../contexts/LanguageContext";
+import { useDarkMode } from "../contexts/DarkModeContext";
+
+import linkedinLogo from "../assets/linkedin.svg";
+import githubLogo from "../assets/github.svg";
+import profilePic from "../assets/images/foto.jpg";
+import emoji from "../assets/images/wavinghand.png";
 
 export default function Hero (){
     const { translations, language } = useContext(LanguageContext);
@@ -16,14 +19,14 @@ export default function Hero (){
     return(
         <>
         <main className="px-10 py-1 md:px-60 md:pb-20 gap-4 md:gap-x-20 grid md:grid-cols-3 items-center ">
-            <section className="md:col-span-2 md:pl-10 ">
-                <h1 className="text-xl mt-4 tracking-wider">{greeting}</h1>
+            <section className="md:col-span-2 md:pl-10">
+                <h1 className="flex text-2xl mt-4 tracking-wider">{greeting} <img src={emoji} alt="" /></h1>
                 <div className="relative">
                     <p className="md:leading-loose text-2xl md:text-4xl mt-4 mb-8 tracking-wide font-medium  relative z-10">
                         {intro}  
                     </p>
                     <div className="absolute inset-0 flex items-center justify-center">
-                        <div className="bg-pink2 rounded-md absolute w-24 md:w-32 h-4 md:h-8 left-0 md:-left-5 bottom-[-2rem] top-5"></div>
+                        <div className="bg-pink2 rounded-md absolute w-24 md:w-40 h-4 md:h-8 left-0 md:-left-5 bottom-[-2rem] top-10"></div>
                     </div> 
                 </div>
                 <div className={darkMode ? "flex gap-4 mt-4 stroke-white" : "flex gap-4 mt-4"}>
