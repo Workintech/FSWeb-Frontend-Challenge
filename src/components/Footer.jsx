d87daa58ce28be4b64b6e1ebdc3c8d45a807584b
ilsu-sunal <sunalilsu@gmail.com>
2024-03-12 15:53:28 +0300

    tüm içerik dile bağlı olarak güncelleniyor

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,18 @@
+import { LanguageContext } from "../contexts/LanguageContext"
+import React, { useContext } from "react"
+
 export default function Footer (){
-    //Buradaki içerik maple yazılacak
+    const { translations } = useContext(LanguageContext);
+    const { goodbye} = translations;
+
     return(
         <>
         <main className="px-10 py-10 md:px-60 md:py-20  md:grid md:grid-cols-3 gap-10">
-            <p className="col-span-2 text-3xl md:text-4xl mt-4 md:mb-8 tracking-wide leading-loose font-medium md:pl-20 text-right pr-10">Let’s work together on your next product.</p>
+            <p className="col-span-2 text-3xl md:text-4xl mt-4 md:mb-8 tracking-wide leading-loose font-medium md:pl-20 text-right pr-10">
+                {goodbye}</p>
             <div className="flex justify-around gap-4 mt-4 md:flex-col md:justify-center font-medium">
                 <a className="text-sky-600" href="">Github</a>
-                <a className="text-black" href="">Personal Blog</a>
+                <a className="text-black" href="">Blog</a>
                 <a className="text-cyan-800" href="">Linkedin</a>
                 <a className="text-pink2" href="">Email</a>
             </div> 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,32 +4,15 @@ import { useContext } from "react"
 
 export default function Projects (){
     const { translations } = useContext(LanguageContext);
-    const projectsTitle = translations.projectsTitle;
 
-    const projectsData = [
-        {
-            title: "Random Jokes",
-            description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia.",
-            tags: ["react", "vercel", "axios", "router"],
-            githubLink: "#",
-            appLink: "#"
-        },
-        {
-            title: "Are you bored?",
-            description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia minima accusamus ratione soluta aperiam sit voluptate? Dicta quod deserunt quam temporibus cumque magnam!",
-            tags: ["react", "redux", "axios", "router", "vercel"],
-            githubLink: "#",
-            appLink: "#"
-        }
-    ];
+    const { projectsTitle, projects } = translations;
 
-    
     return(
         <>
         <main className="px-10 py-10 md:px-60 md:py-20 flex flex-col items-center">
             <h2 className="text-4xl tracking-wide font-medium pb-10">{projectsTitle}</h2>
             <div className=" grid md:grid-cols-2 gap-10">
-            {projectsData.map((project, index) => (
+            {projects.map((project, index) => (
                 <section key={index} className={index % 2 === 0 ? "bg-bluecard rounded-2xl w-full px-6 py-8 relative" : "bg-greencard rounded-2xl w-full px-6 py-8 relative"}>
                     <div className="">
                         <h2 className="text-3xl font-medium py-4">{project.title}</h2>
@@ -40,8 +23,8 @@ export default function Projects (){
                                 ))}
                         </div>
                         <div className="flex justify-between text-xl font-medium py-4 mb-40 md:mb-60">
-                            <a href={project.githubLink}>View on Github</a>
-                            <a href={project.appLink}>Go to App -</a>
+                            <a href={project.githubLink}>{project.github}</a>
+                            <a href={project.appLink}>{project.app}</a>
                         </div>  
                     </div>
                     <img src={computerImage} className="absolute bottom-0 w-full left-1/2 transform -translate-x-1/2 translate-y-1/4" alt="" />
