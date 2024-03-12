b27c7d03dfe2235a61103deddece5ecf611db92b
ilsu-sunal <sunalilsu@gmail.com>
2024-03-12 15:38:09 +0300

    profile kısmındaki data düzeltildi

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,10 @@
 import { LanguageContext } from "../contexts/LanguageContext"
-import { useContext } from "react"
+import React, { useContext } from "react"
 
 export default function Profile (){
-    //Buradaki içerik state olarak gelecek
     const { translations } = useContext(LanguageContext);
 
-    const { profileTitle, infoTitle, about, aboutInfo } = translations;
-    //const info = translations.info
+    const { profileTitle, infoTitle, about, aboutInfo, info } = translations;
 
     return(
         <>
@@ -16,11 +14,13 @@ export default function Profile (){
                 <section className="relative">
                     <div className="bg-white rounded-2xl px-4 py-6 relative z-10">
                         <h2 className="text-m text-pink2 font-medium py-4 z-10">{infoTitle}</h2>
-                        <div className="grid grid-cols-2 gap-4 z-10">
-                         <dt className="font-bold">a</dt> <dd>a</dd>
-                            <dt className="font-bold">a</dt> <dd>a</dd>
-                            <dt className="font-bold">a</dt> <dd>a</dd>
-                            <dt className="font-bold">a</dt> <dd>a</dd>
+                        <div className="grid grid-cols-2 gap-4 p-6 z-10">
+                            {Object.keys(info).map((key) => (
+                                <React.Fragment key={key}>
+                                    <dt className="font-bold">{key}</dt>
+                                    <dd>{info[key]}</dd>
+                                </React.Fragment>
+                            ))}
                         </div> 
                     </div>
                     <div className="absolute inset-0 flex items-center justify-center ">
@@ -33,8 +33,7 @@ export default function Profile (){
                         <div className="absolute inset-0 flex items-center justify-center z-0">
                             <div className="bg-blue2 rounded-md absolute w-18 md:w-24 h-4 left-0 md:-left-5 bottom-[-2rem] top-8"></div>
                         </div> 
-                    </div>
-                    
+                    </div>                   
                     <p>{aboutInfo}</p>
                 </section>
             </div> 
