bbefa47aae066d0be612bcb660de29f5040830ae
ilsu-sunal <sunalilsu@gmail.com>
2024-03-13 18:51:53 +0300

    Header css düzenlendi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,9 +15,9 @@ export default function Header (){
                 <input type="checkbox" value="" className="sr-only peer" checked={theme === "dark"} onChange={toggleTheme}/>
                 <div className={"relative p-2 w-11 h-6 rounded-full peer dark:bg-pink2 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full  after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-yellow2  after:rounded-full after:h-5 after:w-5 after:transition-all  peer-checked:bg-black"}></div>
                 <span className={theme === "dark" ? "ms-3 text-sm font-semibold text-white ":"ms-3 text-sm font-semibold text-gri2 "}>
-                {theme === "dark" && language === "tr" ? "AYDINLIK" :
+                {theme === "dark" && language === "tr" ? "GÜNDÜZ" :
                 theme === "dark" && language === "en" ? "LIGHT MODE" :
-                theme !== "dark" && language === "tr" ? "KARANLIK" :
+                theme !== "dark" && language === "tr" ? "GECE" :
                 "DARK MODE"}
                 </span>
             </label>
